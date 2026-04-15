0a9ae35e9f522d461d464ca127ea3c6a91c2de90
yaroslavvoropaev <voropaevaroslav37@gmail.com>
2026-04-15 12:14:38 +0300

    add input via buffer

--- lab3/lab3.s
+++ lab3/lab3.s
@@ -3,46 +3,48 @@
 %define SYS_OPEN    2
 %define SYS_CLOSE   3
 %define SYS_EXIT    60
+%define BUF_SIZE    4096    
 
 section .data
     msg_f       db "Имя файла: "
     len_f       equ $ - msg_f
     msg_n       db "Сдвиг N: "
     len_n       equ $ - msg_n
-    msg_t       db "Текст (Ctrl+D для завершения):", 10
+    msg_t       db "Текст:", 10
     len_t       equ $ - msg_t
-    
+ 
     space       db ' '
     newline     db 10
 
 section .bss
-    filename    resb 256      
-    n_str       resb 32       ; сдвиг как строка
-    shift_n     resq 1        ; сдвиг как число 
-    fd_out      resq 1        ; файловый дескриптор
+    filename    resb 256      ; под имя файла
+    n_str       resb 32       ; строка для ввода сдвига
+    shift_n     resq 1        ; число сдвига
+    fd_out      resq 1        ; файловый дескриптор 
     
-    char_buf    resb 1        
-    word_buf    resb 1048576  ; длина одного слова как строка
-    word_len    resq 1        ; длина одного слова как число
+    in_buf      resb BUF_SIZE ; буфер для ввода
+    in_pos      resq 1        ; текущая позиция чтения в буфере
+    in_bytes    resq 1        ; количество прочитанных байт в буфере
+
+    word_buf    resb 1048576  ; буфер для одного слова
+    word_len    resq 1        ; длина одного слова
     is_first    resb 1
 
 section .text
     global _start
 
 _start :
-
-
     ; ввод имени файла
-    mov rax, SYS_WRITE         ; номер системеного вызова в rax
-    mov rdi, 1                 ; 1, так как stdout
-    mov rsi, msg_f             ; куда смотреть (адрес)
-    mov rdx, len_f             ; сколько взять байт 
+    mov rax, SYS_WRITE
+    mov rdi, 1
+    mov rsi, msg_f
+    mov rdx, len_f
     syscall
 
     mov rdi, filename
     call read_line  
 
-    ; Ввод числа сдвига
+    ; ввод числа сдвига
     mov rax, SYS_WRITE
     mov rdi, 1
     mov rsi, msg_n
@@ -56,12 +58,12 @@ _start :
     ; открытие файла
     mov rax, SYS_OPEN
     mov rdi, filename
-    mov rsi, 577            ; флгаи O_WRONLY | O_CREAT | O_TRUNC =  1 + 64 + 512 = 577
-    mov rdx, 420            ; прав 0644 (rw-r--r--) в восьм = 420 в дес
+    mov rsi, 577            ; флаги O_WRONLY | O_CREAT | O_TRUNC = 577
+    mov rdx, 420            ; права 0644 (rw-r--r--) в восьм = 420 в дес
     syscall
-    mov [fd_out], rax       ; дескиптор - индекс в таблице ядра
+    mov [fd_out], rax
 
-    ; ввода текста               
+    ; ввод текста для сдвига              
     mov rax, SYS_WRITE
     mov rdi, 1
     mov rsi, msg_t
@@ -71,24 +73,17 @@ _start :
     mov byte [is_first], 1        ; первое слово в строке
     mov qword [word_len], 0       ; длина текущего слова
 
-    ; читаем по одному слову
+    ; читаем по одному слову через буфер
 read_loop:
-    mov rax, SYS_READ
-    mov rdi, 0                     ; 0, так как stdin
-    mov rsi, char_buf
-    mov rdx, 1
-    syscall
-    
-    cmp rax, 0          
-    jle eof                  ;if (eof) 
+    call get_char
+    jc eof              ; если установлен carry  флаг (CF), значит eof
 
-    mov al, [char_buf]
     cmp al, ' '         ; пробел
     je handle_space
     cmp al, 9           ; \t
     je handle_space
     cmp al, 10          ; \n
-    je handle_nl
+    je handle_new_line
 
     ; иначе обычный символ
     mov rcx, [word_len]
@@ -100,7 +95,7 @@ handle_space:
     call flush_word     ; записываем накопленное слово
     jmp read_loop
 
-handle_nl:
+handle_new_line:
     call flush_word     ; записываем слово
 
     mov rax, SYS_WRITE
@@ -112,8 +107,8 @@ handle_nl:
     jmp read_loop
 
 eof:
-    call flush_word     ; записываем наколпенное слово перед выходом
-    
+    call flush_word     ; записываем накопленное слово перед выходом
+
     ; закрываем файл
     mov rax, SYS_CLOSE
     mov rdi, [fd_out]
@@ -124,41 +119,66 @@ eof:
     xor rdi, rdi
     syscall
 
+; функция получения 1 символа из буфера
+get_char:
+    mov rax, [in_pos]       ; в rax позиция в буфере
+    cmp rax, [in_bytes]     
+    jl .fetch_from_buf      ; если в буфере еще есть данные, берем оттуда
 
-; вход: rdi - адреc буфера
+    ; читаем новую порцию
+    mov rax, SYS_READ
+    mov rdi, 0              ; 0, так как stdin
+    mov rsi, in_buf         ; адрес буфера
+    mov rdx, BUF_SIZE       ; размер
+    syscall
+
+    cmp rax, 0
+    jle .eof                ; if (eof) 
+
+    mov [in_bytes], rax     ; cохраняем количество прочитанных байт
+    mov qword [in_pos], 0   ; cбрасываем позицию на начало буфера
+
+.fetch_from_buf:
+    mov rcx, [in_pos]
+    mov al, [in_buf + rcx]  ; достаем символ
+    inc rcx
+    mov [in_pos], rcx
+    clc                        ; oчищаем carray флаг (CF=0)  (успех)
+    ret 
+.eof:
+    stc                     ; устанавливаем carry флаг (CF=1) (не успех)
+    ret
+
+; вход: rdi - адрес буфер
 read_line:
-    push r12                ; r12-r15 non-volatile
+    push r12                ; сохраняем регист
     mov r12, rdi            ; записали в r12 адрес буфера
 .read_line_loop:
-    mov rax, SYS_READ       
-    mov rdi, 0              ; 0, так как stdin
-    mov rsi, char_buf       ; адрес буфера 
-    mov rdx, 1              ; количество считываемых байт 
-    syscall
+    call get_char           ; символ из буфера
+    jc .read_line_done      ; if (eof) 
     
-    cmp rax, 0
-    jle .read_line_done
-    mov al, [char_buf]
-    cmp al, 10
+    cmp al, 10              ; if (\n)
     je .read_line_done
     
     mov [r12], al
     inc r12
     jmp .read_line_loop
 .read_line_done:
-    mov byte [r12], 0
+    mov byte [r12], 0       ; нуль-терминатор
     pop r12
     ret
 
-; Перевод строки в число N
+; перевод строки в число N
 parse_str_to_num:
-    mov rsi, n_str              
+    mov rsi, n_str
     xor rax, rax
     xor rcx, rcx
 .parse_loop:
-    movzx rcx, byte [rsi]     ; читаем байт
+    movzx rcx, byte [rsi]
+
     test rcx, rcx             
-    jz .parse_done            ; if (rcx == 0) (то есть \0)
+    jz .parse_done
+
     sub rcx, '0'
     imul rax, 10
     add rax, rcx
@@ -168,7 +188,7 @@ parse_str_to_num:
     mov [shift_n], rax
     ret
 
-; сдвиг накопленного слова и запись его в файл
+; сдвиг и запись накоплненного слова
 flush_word:
     mov rcx, [word_len]
     test rcx, rcx       
@@ -182,30 +202,26 @@ flush_word:
     mov rdi, [fd_out]
     mov rsi, space
     mov rdx, 1
-    push rcx            ; сохранить rcx (sys_write его затирает)
+    push rcx
     syscall
     pop rcx
 
 .calc_shift:
-    ; расчет сдвига: n % длина
     mov rax, [shift_n]
     xor rdx, rdx
     div rcx
     
-    ; rdx - сколько символов нужно отрубить с конца (хвост)
-    ; rcx - полная длина слова
     mov r8, rdx
     mov r9, rcx
-    sub r9, r8          ; r9 = длина левой части, которая останется на месте
+    sub r9, r8          ; r9 = длина левой части
 
-    ; выводим хвост (правую часть)
     test r8, r8
-    jz .fw_left       ; если хвоста нет
+    jz .fw_left         ; если хвоста нет
     
     mov rax, SYS_WRITE
     mov rdi, [fd_out]
     mov rsi, word_buf
-    add rsi, r9         ; Смещаемся к хвосту
+    add rsi, r9         ; смещаемся к хвосту
     mov rdx, r8
     push r8
     push r9
@@ -214,7 +230,6 @@ flush_word:
     pop r8
 
 .fw_left:
-    ; выводим левую часть
     test r9, r9
     jz .fw_end
 
@@ -225,7 +240,7 @@ flush_word:
     syscall
 
 .fw_end:
-    mov byte [is_first], 0   ; следующее слово уже не первое
-    mov qword [word_len], 0  ; очищаем буфер слова для следующего
+    mov byte [is_first], 0
+    mov qword [word_len], 0
 .fw_ret:
     ret
